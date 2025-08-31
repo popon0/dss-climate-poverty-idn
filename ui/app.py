c5dbc7dc832b575eeb5267ccc9d48be15e3e9e71
Teuku Hafiez Ramadhan <51335497+popon0@users.noreply.github.com>
2025-08-31 17:14:52 +0700

    Add Apache 2.0 license
    - Added LICENSE file with Apache License 2.0
    - Updated README.md with license section
    - Added license headers to main source files

--- ui/app.py
+++ ui/app.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Teuku Hafiez Ramadhan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # app.py
 from __future__ import annotations
 import os
@@ -135,3 +149,4 @@ else:  # Perbandingan
     render_compare(df, df_year, df_range, (start, end), year, TH, prov_list, kpi, tariff, targets)
     render_revenue_compare(df, year, TH, provinces=prov_list)
     render_heatmap_and_optimizer(df, TH)
+
